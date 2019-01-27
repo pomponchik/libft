2ff32ce5174958a33b009e71f0462c0b78fdb55b
pomponchik <34486915+pomponchik@users.noreply.github.com>
2019-01-27 23:08:37 +0300

    Add files via upload

---
+++ ft_binary_lst_search_cs.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+t_two_list *ft_binary_lst_strdel(t_two_list *alst, size_t to_search)
+{
+  t_two_list *temp;
+
+	if (!alst)
+		return (NULL);
+  temp = alst;
+  while (temp)
+  {
+    if (temp->content_size == to_search)
+      return (temp);
+    temp = temp->next;
+  }
+  temp = alst->back;
+  while (temp)
+  {
+    if (temp->content_size == to_search)
+      return (temp);
+    temp = temp->back;
+  }
+  return (NULL);
+}
