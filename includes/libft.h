78e0d4b5f4c951f24c9bdcb513c0ef09479e3464
Annara farring Halmon paege <ahalmon-@e3r5p4.21-school.ru>
2019-03-15 03:07:42 +0300

    add function

--- includes/libft.h
+++ includes/libft.h
@@ -143,5 +143,6 @@ long int				ft_math_rounding_down_float(float num);
 long long int			ft_math_rounding_down_double(double num);
 long long int			ft_math_rounding_down_long_double(long double num);
 int						ft_math_numlen_long(long int num);
+size_t					ft_math_how_long_this_decimal_long_double(long double num);
 
 #endif
