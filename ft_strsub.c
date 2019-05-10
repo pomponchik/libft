e50a977d64dea3dbfc51eff57982774ba90ba77f
Annara farring Halmon paege <ahalmon-@il-h5.21-school.ru>
2019-05-10 13:53:57 +0300

    microfix

--- ft_strsub.c
+++ ft_strsub.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char		*ft_strsub(char const *s, unsigned int start, size_t len)
 {
