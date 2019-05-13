23e299828a7b0d279d8a86a3f71cae420d40981d
Annara farring Halmon paege <ahalmon-@il-h5.21-school.ru>
2019-05-13 15:52:44 +0300

    microfix

--- ft_lst_strjoin_free.c
+++ ft_lst_strjoin_free.c
@@ -18,20 +18,23 @@ char		*ft_lst_strjoin_fr(t_list *lst)
 	void	*temp;
 	size_t	size;
 	t_list	*t;
+	size_t max;
 
 	if (!lst || !(result = (char *)malloc(ft_lst_strlen(lst) + 1)))
 		return (NULL);
 	temp = (void *)result;
+	max = 0;
 	while (lst)
 	{
 		t = lst;
 		size = ft_strlen((char *)lst->content);
+		max += size;
 		ft_memcpy(temp, lst->content, size);
 		temp = ft_jump_pointer_size_t(temp, size);
 		lst = lst->next;
 		free(t->content);
 		free(t);
 	}
-	*((char *)temp) = '\0';
+	result[max] = '\0';
 	return (result);
 }
