372620e4a2055bad4bef06ddf52932687cccbc31
Annara farring Halmon paege <ahalmon-@at-g3.21-school.ru>
2019-05-22 16:53:55 +0300

    microfix

--- ft_lst_strjoin_counter_out.c
+++ ft_lst_strjoin_counter_out.c
@@ -25,8 +25,11 @@ char		*ft_lst_strjoin_counter_out(t_list *lst, size_t *counter)
 	while (lst)
 	{
 		size = ft_strlen_safe((char *)lst->content);
-		*counter += size;
-		ft_memcpy_safe(temp, lst->content, size);
+		if (size)
+		{
+			*counter += size;
+			ft_memcpy_safe(temp, lst->content, size);
+		}
 		temp = ft_jump_pointer_size_t(temp, size);
 		lst = lst->next;
 	}
